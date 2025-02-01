fcf690badb585c6e5089413c27b69659cc30c6ae
Marina <pchupchu@yandex.ru>
2025-02-01 12:34:44 +0300

    Add images and feature components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/sections/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '2rem',
+        lg: '4rem',
       },
+    },
+    fontFamily: {
+      sans: ['var(--font-inter)', 'sans-serif'],
+    },
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1200px',
     },
   },
   plugins: [],
