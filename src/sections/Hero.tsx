dcc9f576341db283fbe264b673bc8da5ef04388c
Marina <pchupchu@yandex.ru>
2025-02-03 11:47:51 +0300

    Add Hero animation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,26 +1,95 @@
-import Image from 'next/image';
+'use client';
 
+import Image from 'next/image';
+import { useEffect } from 'react';
+
+import { motion, useAnimate } from 'framer-motion';
+
+import cursorYouImage from '@/assets/images/cursor-you.svg';
 import desighExampleImage1 from '@/assets/images/design-example-1.png';
 import desighExampleImage2 from '@/assets/images/design-example-2.png';
 import Button from '@/components/Button';
 import Pointer from '@/components/Pointer';
 
 export default function Hero() {
+  const [leftDesignScope, leftDesignAnimate] = useAnimate();
+  const [leftPointerScope, leftPointerAnimate] = useAnimate();
+  const [rightDesignScope, rightDesignAnimate] = useAnimate();
+  const [rightPointerScope, rightPointerAnimate] = useAnimate();
+
+  useEffect(() => {
+    leftDesignAnimate([
+      [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ]);
+    leftPointerAnimate([
+      [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+      [
+        leftPointerScope.current,
+        { y: [0, 16, 0], x: 0 },
+        { duration: 0.5, ease: 'easeInOut' },
+      ],
+    ]);
+    rightDesignAnimate([
+      [rightDesignScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+      [rightDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ]);
+    rightPointerAnimate([
+      [
+        rightPointerScope.current,
+        { opacity: 1 },
+        { duration: 0.5, delay: 1.5 },
+      ],
+      [rightPointerScope.current, { y: 0, x: 175 }, { duration: 0.5 }],
+      [
+        rightPointerScope.current,
+        { y: [0, 20, 0], x: 0 },
+        { duration: 0.5, ease: 'easeInOut' },
+      ],
+    ]);
+  }, []);
+
   return (
-    <section className='overflow-clip py-24'>
+    <section
+      className='overflow-x-clip py-24'
+      style={{ cursor: `url(${cursorYouImage.src}), auto` }}>
       <div className='container relative'>
-        <div className='absolute -left-32 top-16 hidden lg:block'>
-          <Image src={desighExampleImage1} alt='Design Example 1' />
-        </div>
-        <div className='absolute -right-64 -top-16 hidden lg:block'>
-          <Image src={desighExampleImage2} alt='Design Example 2' />
-        </div>
-        <div className='absolute left-56 top-96 hidden lg:block'>
+        <motion.div
+          className='absolute -left-32 top-16 hidden lg:block'
+          ref={leftDesignScope}
+          initial={{ opacity: 0, y: 100, x: -100 }}
+          drag>
+          <Image
+            src={desighExampleImage1}
+            alt='Design Example 1'
+            draggable={false}
+          />
+        </motion.div>
+        <motion.div
+          className='absolute left-56 top-96 hidden lg:block'
+          ref={leftPointerScope}
+          initial={{ opacity: 0, y: 100, x: -200 }}>
           <Pointer name='Andrea' />
-        </div>
-        <div className='absolute -top-4 right-80 hidden lg:block'>
+        </motion.div>
+
+        <motion.div
+          className='absolute -right-64 -top-16 hidden lg:block'
+          ref={rightDesignScope}
+          initial={{ opacity: 0, y: 100, x: 100 }}
+          drag>
+          <Image
+            src={desighExampleImage2}
+            alt='Design Example 2'
+            draggable={false}
+          />
+        </motion.div>
+        <motion.div
+          className='absolute -top-4 right-80 hidden lg:block'
+          ref={rightPointerScope}
+          initial={{ opacity: 0, y: 100, x: 275 }}>
           <Pointer name='Bryan' color='red' />
-        </div>
+        </motion.div>
         <div className='flex justify-center'>
           <div className='inline-flex rounded-full bg-gradient-to-r from-purple-400 to-pink-400 px-3 py-1 font-semibold text-neutral-950'>
             ✨ $7.5M seed round raised
