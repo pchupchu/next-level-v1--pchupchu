03d21c4be4a8b80f879d3a880c58ff18d55304b9
Marina <pchupchu@yandex.ru>
2025-02-03 13:50:29 +0300

    Add Feature animation

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import avatar1 from '@/assets/images/avatar-ashwin-santiago.jpg';
 import avatar3 from '@/assets/images/avatar-florence-shaw.jpg';
 import avatar2 from '@/assets/images/avatar-lula-meyers.jpg';
+import avatar4 from '@/assets/images/avatar-owen-garcia.jpg';
 import Avatar from '@/components/Avatar';
 import FeatureCard from '@/components/FeatureCard';
 import Key from '@/components/Key';
@@ -30,7 +31,7 @@ export default function Features() {
         </h2>
         <div className='mt-12 grid grid-cols-1 gap-8 md:grid-cols-4 lg:grid-cols-3'>
           <FeatureCard
-            className='md:col-span-2 lg:col-span-1'
+            className='group md:col-span-2 lg:col-span-1'
             title='Real-time Collaboration'
             description='Work together seamlessly with conflict-free team editing'>
             <div className='flex aspect-video items-center justify-center'>
@@ -43,8 +44,13 @@ export default function Features() {
               <Avatar className='z-20 -ml-6 border-amber-500'>
                 <Image className='rounded-full' src={avatar3} alt='avatar' />
               </Avatar>
-              <Avatar className='-ml-6 border-transparent'>
-                <div className='inline-flex size-full items-center justify-center gap-1 rounded-full bg-neutral-700'>
+              <Avatar className='-ml-6 border-transparent transition group-hover:border-green-500'>
+                <div className='relative inline-flex size-full items-center justify-center gap-1 rounded-full bg-neutral-700'>
+                  <Image
+                    className='absolute size-full rounded-full opacity-0 transition group-hover:opacity-100'
+                    src={avatar4}
+                    alt='avatar'
+                  />
                   {Array.from({ length: 3 }).map((_, i) => {
                     return (
                       <span
@@ -57,27 +63,40 @@ export default function Features() {
             </div>
           </FeatureCard>
           <FeatureCard
-            className='md:col-span-2 lg:col-span-1'
+            className='group md:col-span-2 lg:col-span-1'
             title='Interactive Prototyping'
             description='Engage your clients with prototypes that react to user actions'>
             <div className='flex aspect-video items-center justify-center'>
-              <p className='text-center text-4xl font-extrabold text-white/20'>
+              <p className='text-center text-4xl font-extrabold text-white/20 transition duration-500 group-hover:text-white/10'>
                 We&apos;ve achieved{' '}
-                <span className='bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent'>
-                  incredible
+                <span className='relative bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent'>
+                  <span>incredible</span>
+                  <video
+                    className='pointer-events-none absolute bottom-full left-1/2 -translate-x-1/2 rounded-2xl opacity-0 shadow-xl transition duration-500 group-hover:opacity-100'
+                    src='/assets/gif-incredible.mp4'
+                    autoPlay
+                    loop
+                    muted
+                    playsInline></video>
                 </span>{' '}
                 growth this year
               </p>
             </div>
           </FeatureCard>
           <FeatureCard
-            className='md:col-span-2 md:col-start-2 lg:col-span-1 lg:col-start-auto'
+            className='group md:col-span-2 md:col-start-2 lg:col-span-1 lg:col-start-auto'
             title='Keyboard Quick Actions'
             description='Powerful commands to help you create designs more quickly'>
             <div className='flex aspect-video items-center justify-center gap-4'>
-              <Key className='w-28'>shift</Key>
-              <Key>alt</Key>
-              <Key>C</Key>
+              <Key className='w-28 outline-2 outline-offset-4 outline-lime-400 transition-all duration-100 group-hover:translate-y-1 group-hover:outline'>
+                shift
+              </Key>
+              <Key className='outline-2 outline-offset-4 outline-lime-400 transition-all delay-150 duration-100 group-hover:translate-y-1 group-hover:outline'>
+                alt
+              </Key>
+              <Key className='outline-2 outline-offset-4 outline-lime-400 transition-all delay-300 duration-100 group-hover:translate-y-1 group-hover:outline'>
+                C
+              </Key>
             </div>
           </FeatureCard>
         </div>
@@ -86,8 +105,8 @@ export default function Features() {
             return (
               <div
                 key={feature}
-                className='flex items-center gap-3 rounded-2xl border border-white/10 bg-neutral-900 px-3 py-1.5 md:px-5 md:py-2'>
-                <span className='inline-flex size-5 items-center justify-center rounded-full bg-lime-400 text-xl text-neutral-950'>
+                className='group flex items-center gap-3 rounded-2xl border border-white/10 bg-neutral-900 px-3 py-1.5 transition duration-500 hover:scale-105 md:px-5 md:py-2'>
+                <span className='inline-flex size-5 items-center justify-center rounded-full bg-lime-400 text-xl text-neutral-950 transition duration-500 group-hover:rotate-45'>
                   &#10038;
                 </span>
                 <span className='font-medium md:text-lg'>{feature}</span>
